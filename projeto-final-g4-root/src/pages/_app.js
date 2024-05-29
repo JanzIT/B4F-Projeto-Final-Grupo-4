73040458c9ea7a120761b25376a8095a16dcbed2
Karol <karolainemorgado@gmail.com>
2024-05-29 20:01:56 +0100

    Remove ponto e virgula no app.js

--- projeto-final-g4-root/src/pages/_app.js
+++ projeto-final-g4-root/src/pages/_app.js
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }) {
   return (
     <NextUIProvider>
       <div className={`${urbanist.className}`}>
-        <Component {...pageProps} />;
+        <Component {...pageProps} />
       </div>
     </NextUIProvider>
   );
