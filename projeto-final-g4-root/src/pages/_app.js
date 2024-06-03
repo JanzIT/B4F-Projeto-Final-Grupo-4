035cab7096854b9e5faa6f68f073a3508c4c06da
Renan Janz H Vieira <janzhv.it@gmail.com>
2024-06-03 16:44:02 +0100

    Vercel changes

--- projeto-final-g4-root/src/pages/_app.js
+++ projeto-final-g4-root/src/pages/_app.js
@@ -1,14 +1,30 @@
 import "@/styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
-
 import { Urbanist } from "next/font/google";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const [redirected, setRedirected] = useState(false);
+
+  useEffect(() => {
+    if (!redirected) {
+      const storedUser = JSON.parse(localStorage.getItem("user"));
+      if (!storedUser || !storedUser.user) {
+        router.push("/auth");
+      } else {
+        router.push("/dashboard");
+      }
+      setRedirected(true);
+    }
+  }, [redirected, router]);
+
   return (
     <NextUIProvider>
-      <div className={`${urbanist.className}`}>
+      <div className={urbanist.className}>
         <Component {...pageProps} />
       </div>
     </NextUIProvider>
